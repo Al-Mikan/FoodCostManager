575ca6a21c9dcaa314cfce199acbc131f34b09cb
AlMikan <nekomimi987@gmail.com>
2023-06-09 23:59:50 +0900

    Initial Commit

---
+++ FoodCostManager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FoodCostManager
+//
+//  Created by 菊地真優 on 2023/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodCostManager/FoodCostManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCostManagerApp.swift
+//  FoodCostManager
+//
+//  Created by 菊地真優 on 2023/06/09.
+//
+
+import SwiftUI
+
+@main
+struct FoodCostManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
